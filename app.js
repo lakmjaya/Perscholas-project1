cabc20605226a952f4e2f3af9a690ac244ecb6be
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-11-09 15:41:35 -0500

    second

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ function startGame() {
     document.getElementById("myImg").src = "cards/"+x+".svg";
   }
 function play1Section() {
-    let number =parseInt(document.getElementById("num").value)  ;
-    console.log(number)
-    //let type = parseInt(document.getElementById("mySelect").value);
+    // let number =parseInt(document.getElementById("num").value)  ;
+    // console.log(number)
+    // let sign = parseInt(document.getElementsById("sign").value);
+    // console.log(sign)
+    // let y = 4 //number + sign
+    // console.log(y)
+    document.getElementById("play1Card").src = "cards/6.svg";
+  }
+
+  function play2Section() {
+    // let number =parseInt(document.getElementById("num1").value)  ;
+    // console.log(number)
+    // let sign = parseInt(document.getElementsById("sign1").value);
+    // console.log(sign)
+    // let y = 5 //number + sign
+    // console.log(y)
+    document.getElementById("play2Card").src = "cards/4.svg";
   }

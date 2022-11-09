625068df72e939f5296efb285c1552d70e157352
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-11-09 16:06:10 -0500

    Third

--- app.js
+++ app.js
@@ -2,22 +2,10 @@ function startGame() {
     let x = Math.floor(Math.random()*52)+1
     document.getElementById("myImg").src = "cards/"+x+".svg";
   }
-function play1Section() {
-    // let number =parseInt(document.getElementById("num").value)  ;
-    // console.log(number)
-    // let sign = parseInt(document.getElementsById("sign").value);
-    // console.log(sign)
-    // let y = 4 //number + sign
-    // console.log(y)
+  document.getElementById("plyer1-selection").addEventListener("click", function play1Section() {
     document.getElementById("play1Card").src = "cards/6.svg";
-  }
+  });
 
   function play2Section() {
-    // let number =parseInt(document.getElementById("num1").value)  ;
-    // console.log(number)
-    // let sign = parseInt(document.getElementsById("sign1").value);
-    // console.log(sign)
-    // let y = 5 //number + sign
-    // console.log(y)
-    document.getElementById("play2Card").src = "cards/4.svg";
+        document.getElementById("play2Card").src = "cards/4.svg";
   }

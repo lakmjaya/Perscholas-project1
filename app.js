9ca702c741a298fea0aeae46b53f73553da662f8
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-11-15 16:42:18 -0500

    seventh commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
   let y =0
   let z=0
   let ply1 = document.getElementById("plyer1-selection")
-  
+  document.getElementById("plyer1-lock").disabled = true
+  document.getElementById("plyer2-lock").disabled = true
   function play1Section() {
     let number =0
     var val=0
@@ -18,6 +19,7 @@
     if(number !=0 && val !=0){
       y = number + val
       document.getElementById("play1Card").src = "cards/"+y+".svg";
+      document.getElementById("plyer1-lock").disabled = false
     }else if(number ==0 && val ==0){
       alert("Player 1 : Please select your favorite betting card number and Sign")
     }else if(number ==0){
@@ -43,6 +45,7 @@
     if(number1 !=0 && val1 !=0){
         z = number1 + val1
         document.getElementById("play2Card").src = "cards/"+z+".svg";
+        document.getElementById("plyer2-lock").disabled = false
       }else if(number1 ==0 && val1 ==0){
         alert("Player 2 : Please select your favorite betting card number and Sign")
       }else if(number1 ==0){
@@ -58,17 +61,28 @@
     document.getElementById("myImg").src = "cards/"+x+".svg";
     if(z===x){
       alert( "Player 2 win the game")
+      document.getElementById("restart").disabled = false
+      document.getElementById("start-selection").disabled = true
     }else if(y===x){
       alert( "Player 1 win the game")
+      document.getElementById("restart").disabled = false
+      document.getElementById("start-selection").disabled = true
     }else if(z===x && y===x){
       alert( " The game again, Plaese refresh the page")
     }
     }
 
     function lockPlayer1(){
-
+      document.getElementById("plyer1-selection").disabled = true
+      document.getElementById("start-selection").disabled = false
     }
 
     function lockPlayer2(){
+      document.getElementById("plyer2-selection").disabled = true
+      document.getElementById("start-selection").disabled = false
+    }
 
+    function restartGame(){
+      document.getElementById("plyer2-selection").disabled = false
+      document.getElementById("plyer2-selection").disabled = false
     }

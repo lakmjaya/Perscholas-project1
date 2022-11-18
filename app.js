4cd8c1a63e8f3e39402703b0e9b93dc9ed6a30ee
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-11-17 22:40:57 -0500

    eight

--- app.js
+++ app.js
@@ -1,88 +1,144 @@
-  let y =0
-  let z=0
-  let ply1 = document.getElementById("plyer1-selection")
-  document.getElementById("plyer1-lock").disabled = true
-  document.getElementById("plyer2-lock").disabled = true
-  function play1Section() {
-    let number =0
-    var val=0
-    number = parseInt( document.getElementById("num").value)
-    var radios = document.getElementsByName('fav_sign');
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked ) {
-        // do whatever you when the checked radio
-        val=parseInt(radios[i].value);
-        console.log(val)
-        break
-      }
-    }
-    if(number !=0 && val !=0){
-      y = number + val
-      document.getElementById("play1Card").src = "cards/"+y+".svg";
-      document.getElementById("plyer1-lock").disabled = false
-    }else if(number ==0 && val ==0){
-      alert("Player 1 : Please select your favorite betting card number and Sign")
-    }else if(number ==0){
-      alert("Player 1 : Please select your favorite betting card number")
-    }else if(val ==0){
-      alert("Player 1 : Please select your favorite betting card sign")
-    }   
-  }
-
-  function play2Section() {
-    var radios1 = document.getElementsByName('fav_sign1');
-    var val1=0
-    let number1 =0
-        number1 = parseInt( document.getElementById("num1").value)
-      for (var i = 0, length = radios1.length; i < length; i++) {
-        if (radios1[i].checked) {
-          // do whatever when the checked radio
-          val1=parseInt(radios1[i].value);
+    let y =0
+    let z=0
+    let lock1 =0
+    let lock2 =0
+    loadPage()
+    let ply1 = document.getElementById("plyer1-selection")
+    
+    function play1Section() {
+      let number =0
+      var val=-1
+      number = parseInt( document.getElementById("num").value)
+      var radios = document.getElementsByName('fav_sign');
+      for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked ) {
+          // do whatever you when the checked radio
+          val=parseInt(radios[i].value);
+          console.log(val)
           break
         }
-        
       }
-    if(number1 !=0 && val1 !=0){
-        z = number1 + val1
-        document.getElementById("play2Card").src = "cards/"+z+".svg";
-        document.getElementById("plyer2-lock").disabled = false
-      }else if(number1 ==0 && val1 ==0){
-        alert("Player 2 : Please select your favorite betting card number and Sign")
-      }else if(number1 ==0){
-        alert("Player 2 : Please select your favorite betting card number")       
-      }else if(val1 ==0) {
-        alert("Player 2 : Please select your favorite betting card sign")
+      if(number !=0 && val >=0){
+        y = number + val
+        document.getElementById("play1Card").src = "cards/"+y+".svg";
+        setVisible("plyer1-lock")
+      }else if(number ==0 && val ==0){
+        alert("Player 1 : Please select your favorite betting card number and Sign")
+      }else if(number ==0){
+        alert("Player 1 : Please select your favorite betting card number")
+      }else if(val < 0){
+        alert("Player 1 : Please select your favorite betting card sign")
+      }   
     }
-  }
-  ply1.addEventListener("click", play1Section) 
+    function loadPage(){
+      x=0
+      y=0
+      z=0
+      lock1=0
+      lock2=0
+      setHidden("myImg")
+      setHidden("restart")
+      setHidden("start-selection")
+      setHidden("plyer1-lock")
+      setHidden("plyer2-lock")
+      setHidden("game")
+    }
+    function play2Section() {
+      var radios1 = document.getElementsByName('fav_sign1');
+      var val1= -1
+      let number1 =0
+          number1 = parseInt( document.getElementById("num1").value)
+        for (var i = 0, length = radios1.length; i < length; i++) {
+          if (radios1[i].checked) {
+            // do whatever when the checked radio
+            val1=parseInt(radios1[i].value);
+            break
+          }  
+        }
+      if(number1 !=0 && val1 >= 0){
+          z = number1 + val1
+          document.getElementById("play2Card").src = "cards/"+z+".svg";
+          setVisible("plyer2-lock")
+        }else if(number1 ==0 && val1 ==0){
+          alert("Player 2 : Please select your favorite betting card number and Sign")
+        }else if(number1 ==0){
+          alert("Player 2 : Please select your favorite betting card number")       
+        }else if(val1 < 0) {
+          alert("Player 2 : Please select your favorite betting card sign")
+      }
+    }
+    ply1.addEventListener("click", play1Section) 
 
-  function startGame() {
-    let x = Math.floor(Math.random()*52)+1
-    document.getElementById("myImg").src = "cards/"+x+".svg";
-    if(z===x){
-      alert( "Player 2 win the game")
-      document.getElementById("restart").disabled = false
-      document.getElementById("start-selection").disabled = true
-    }else if(y===x){
-      alert( "Player 1 win the game")
-      document.getElementById("restart").disabled = false
-      document.getElementById("start-selection").disabled = true
-    }else if(z===x && y===x){
-      alert( " The game again, Plaese refresh the page")
-    }
-    }
+    function startGame() {
+      setVisible("game")
+      document.getElementById("game").innerHTML = "Random Betting Card - Click Again";
+      let x = Math.floor(Math.random()*52)+1
+      document.getElementById("myImg").src = "cards/"+x+".svg";
+      if(z===x && y===x){
+        alert(" Both betting palyers are win the game again, Plaese refresh the page" )
+        setVisible("restart")
+        setHidden("start-selection")
+      }else if(y===x){
+        document.getElementById("game").innerHTML = "Player 1 win the Card - Click Restart to Continue";
+        alert( "Player 1 win the game")
+        setVisible("restart")
+        setHidden("start-selection")
+      }else if(z===x ){
+        document.getElementById("game").innerHTML = "Player 2 win the Card - Click Restart to Continue";
+        alert( "Player 2 win the game")
+        setVisible("restart")
+        setHidden("start-selection")
+      }
+      }
 
-    function lockPlayer1(){
-      document.getElementById("plyer1-selection").disabled = true
-      document.getElementById("start-selection").disabled = false
-    }
+      function disableButton(id){
+        document.getElementById(id).disabled = true
+      }
+      function setVisible(id){
+        document.getElementById(id).style.visibility ="visible"
+      }
+      function setHidden(id){
+        document.getElementById(id).style.visibility ="hidden"
+      }
 
-    function lockPlayer2(){
-      document.getElementById("plyer2-selection").disabled = true
-      document.getElementById("start-selection").disabled = false
-    }
+      function enableButton(id){
+        document.getElementById(id).disabled = false
+      }
 
-    function restartGame(){
-      document.getElementById("plyer2-selection").disabled = false
-      document.getElementById("plyer2-selection").disabled = false
-    }
+      function lockPlayer1(){
+        disableButton("plyer1-selection")
+        lock1 =1
+        if (z===y){
+          alert( "Both Players can not be selected same betting card")
+          setVisible("restart")
+        }else{
+          if(lock1 >0 && lock2 >0){
+            setVisible ("start-selection")
+            setVisible("myImg")
+          }
+        }
+        setHidden("plyer1-lock")
+      }
+
+      function lockPlayer2(){
+        disableButton("plyer2-selection")
+        lock2=1
+        if (z===y){
+          alert( "Both Players can not be selected same betting card")
+          setVisible("restart") 
+        }else{
+          if(lock1 >0 && lock2 >0){
+            setVisible("start-selection")
+            setVisible("myImg")
+          }
+        }
+        setHidden("plyer2-lock")
+      }
+
+      function restartGame(){
+        document.getElementById("play1Card").src = "cards/back.jpg";
+        document.getElementById("play2Card").src = "cards/RED_BACK.svg";
+        enableButton("plyer1-selection")
+        enableButton("plyer2-selection")
+        loadPage()
+        }
